1dbe05be2e786a4366a9486a110f2eb5b5f1238e
Chirag <chirags1725@gmail.com>
2021-12-23 01:18:11 +0530

    init

--- src/Components/AdUnit.js
+++ src/Components/AdUnit.js
@@ -15,6 +15,8 @@ class AdUnit extends Component {
           data-ad-layout-key="-f9+52+6z-e1+5b"
           data-ad-client="ca-pub-6308049796077270"
           data-ad-slot="5574338884"
+          
+     data-full-width-responsive="true"
         ></ins>
       </div>
     );
