aae6d2316fee4414b8c1c8a14385247d7e20592d
Chirag <chirags1725@gmail.com>
2022-01-04 17:07:38 +0530

    init

--- src/Components/Blogs_data.js
+++ src/Components/Blogs_data.js
@@ -14,7 +14,7 @@ const BlogData = [
   {
     id: 3,
     Title: "Python Module - Delorean",
-    desc: "Delorean is a really cool date/time library. Apart from having a sweet name, it's one of the more natural feeling date/time munging libraries I've used in Python. It's sort of like moment in javascript, except I laugh every time I import it. The docs are also good and in addition to being technically helpful, they also make countless Back to the Future references. Install: pip install delorean. Importing: from delorean import Delorean. Print time a s print(Delorean(timezone=EST))",
+    desc: "Delorean is a really cool date/time library. Apart from having a sweet name, it's one of the more natural feeling date/time munging libraries I've used in Python. It's sort of like moment in javascript, except I laugh every time I import it. The docs are also good and in addition to being technically helpful, they also make countless Back to the Future references. Install: pip install delorean. Importing: from delorean import Delorean. Print time as print(Delorean(timezone=EST))",
     image: "3",
   },
   {
@@ -43,32 +43,32 @@ const BlogData = [
   },
   {
     id: 8,
-    Title: "",
-    desc: "blog6",
+    Title: "CSS Property - Mix-blend-mode",
+    desc: "This can be considered as an imae editing tool in css. It has many value such as mix-blend-mode: normal|multiply|screen|overlay|darken|lighten|color-dodge|color-burn|difference|exclusion|hue|saturation|color|luminosity;. It's syntax is as: (element){mix-blend-mode : 'value'}. And  its javascript syntax is: element.style.mixBlendMode = 'value'. It's default value is normal",
     image: "",
   },
   {
     id: 9,
-    Title: "",
-    desc: "blog6",
+    Title: "CSS Property - Opacity",
+    desc: "It defines the opacity of an element. Its value ranges from 0 to 1. Its values are: number(from 0 to 1), inherit, initial. It's syntax is as: (element){opacity : 'value'}. And  its javascript syntax is: element.style.opactiy = 'value'",
     image: "",
   },
   {
     id: 10,
-    Title: "",
-    desc: "blog6",
+    Title: "Python Module - FuzzyWuzzy",
+    desc: "FuzzyWuzzy is a library of Python which is used for string matching. Fuzzy string matching is the process of finding strings that match a given pattern. To install type in command line : 'pip install fuzzywuzzy'. Then import as 'from fuzzywuzzy import fuzz'. Then check the strings as: \"fuzz.ratio('First String here','Second string here')\"",
     image: "",
   },
   {
     id: 11,
-    Title: "",
-    desc: "blog6",
+    Title: "Python Module - PyAztro",
+    desc: "PyAztro provides horoscope info for sun signs such as Lucky Number, Lucky Color, Mood, Color, Compatibility with other sun signs, description of a sign for that day etc. To install, in command line paste the following code: 'pip install pyaztro'. To import: 'import pyaztro'. Then print your daily horoscope as: 'print(pyaztro.Aztro('{your_zodiac_sign}').{ lucky_number, color, mood, lucky_time, date_range, compatibility, current_date})'",
     image: "",
   },
   {
     id: 12,
-    Title: "",
-    desc: "blog6",
+    Title: "Python Module - PyInstaller",
+    desc: "PyInstaller bundles a Python application and all its dependencies into a single package. It basically converts .py(python file) to .exe(executable file). To install, in command line paste the following code: 'pip install pyinstaller'. To import: '{no need to import as this works in command line only}'. To make .py to .exe, in command line paste: pyinstaller {name_of_python_file}.py. Then you can locate your exe file in dist folder",
     image: "",
   },
 ];
